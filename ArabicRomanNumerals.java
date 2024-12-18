76d0a1ababb386b8664b2ba99b7a71fd63586434
Ayoub <ayoubbourti3@gmail.com>
2024-12-19 00:03:17 +0100

    cycle 4

--- ArabicRomanNumerals.java
+++ ArabicRomanNumerals.java
@@ -3,7 +3,8 @@ package nombres;
 public class ArabicRomanNumerals {
 
     public String convert(int i){
-        if(i>1) {return "II" ;}
+        if(i>2){ return "III";}
+        else if(i>1) {return "II" ;}
         return "I";
     }
 }
