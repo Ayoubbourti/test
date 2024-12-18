63233ba8beb9d969b4dea3b286df6f162c864908
Ayoub <ayoubbourti3@gmail.com>
2024-12-19 00:10:49 +0100

    cycle 8

--- ArabicRomanNumerals.java
+++ ArabicRomanNumerals.java
@@ -1,20 +1,16 @@
 package nombres;
 
 public class ArabicRomanNumerals {
-
     public String convert(int nombre) {
         StringBuilder resultat = new StringBuilder();
 
-        if (nombre >= 10) {
-            for (int i = 0; i < nombre/10; i++) {
-                resultat.append("X");
-            }
-        } else {
-            for (int i = 0; i < nombre; i++) {
-                resultat.append("I");
-            }
+        for (int i = 0; i < nombre / 10; i++) {
+            resultat.append("X");
+        }
+        for (int i = 0; i < nombre % 10; i++) {
+            resultat.append("I");
         }
 
         return resultat.toString();
-    }   }
+    }  }
 
